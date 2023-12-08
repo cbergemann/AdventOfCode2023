1c7257d6eaa1ec870f85a7640b6766954cfa8c2f
Constantin Bergemann <cbergemann@nordex-online.com>
2023-12-08 16:10:07 +0100

    add Day2 solution

---
+++ Day2/Game.cs
@@ -0,0 +1,58 @@
+﻿class Game
+{
+    public static Game FromString(string config)
+    {
+        var green = 0;
+        var blue = 0;
+        var red = 0;
+
+        var cubeDraws = config.Split(',');
+        foreach (var cubeDraw in cubeDraws)
+        {
+            var split = cubeDraw.Trim().Split(' ');
+            var count = int.Parse(split[0]);
+            var color = split[1];
+
+            switch (color)
+            {
+                case "green":
+                    green += count;
+                    break;
+                case "blue":
+                    blue += count;
+                    break;
+                case "red":
+                    red += count;
+                    break;
+                default:
+                    throw new Exception();
+            }
+        }
+
+        return new Game(green, blue, red);
+    }
+
+    public Game(int green, int blue, int red)
+    {
+        Green = green;
+        Blue = blue;
+        Red = red;
+    }
+
+    public int Green { get; private set; }
+
+    public int Blue { get; private set; }
+
+    public int Red { get; private set; }
+
+    public bool IsPossible(Game other) => !(other.Green > Green || other.Blue > Blue || other.Red > Red);
+
+    public void Merge(Game other)
+    {
+        Green = Green < other.Green ? other.Green : Green;
+        Blue = Blue < other.Blue ? other.Blue : Blue;
+        Red = Red < other.Red ? other.Red : Red;
+    }
+
+    public long Power => Green * Blue * Red;
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void PartOne()
+{
+    var sum = 0L;
+    var baseGame = new Game(13, 14, 12);
+    foreach (var line in File.ReadAllLines("input.txt"))
+    {
+        var split1 = line.Split(':', 2);
+        var split2 = split1[0].Split(' ');
+        var gameId = int.Parse(split2[1]);
+
+        var draws = split1[1].Split(';');
+        foreach (var draw in draws)
+        {
+            var game = Game.FromString(draw);
+            if (!baseGame.IsPossible(game))
+            {
+                goto not_possible;
+            }
+        }
+
+        sum += gameId;
+
+        not_possible: ;
+    }
+
+    Console.WriteLine($"Solution: {sum}");
+}
+
+void PartTwo()
+{
+    var sum = 0L;
+    foreach (var line in File.ReadAllLines("input.txt"))
+    {
+        var split1 = line.Split(':', 2);
+        var split2 = split1[0].Split(' ');
+        var gameId = int.Parse(split2[1]);
+
+        var draws = split1[1].Split(';');
+        var game = new Game(0, 0, 0);
+        foreach (var draw in draws)
+        {
+            var game2 = Game.FromString(draw);
+            game.Merge(game2);
+        }
+
+        sum += game.Power;
+    }
+
+    Console.WriteLine($"Solution: {sum}");
+}
+
+PartOne();
+PartTwo();
