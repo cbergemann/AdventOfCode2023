50b0de13c37ecf9afd9a10cda3264c0494ebb486
cbe <cbe@cbe>
2023-12-09 17:26:07 +0100

    add Day 9 part 1 solution

---
+++ Day9/History.cs
@@ -0,0 +1,42 @@
+﻿namespace Day9;
+
+public class History
+{
+    private readonly long[] _values;
+
+    public History(string line)
+    {
+        _values = line.Split(' ').Select(long.Parse).ToArray();
+    }
+
+    public History(History parent)
+    {
+        _values = parent._values.Zip(parent._values.Skip(1), (a, b) => b - a).ToArray();
+    }
+
+    public long ExtrapolateNextValue()
+    {
+        var children = new Stack<History>();
+        var history = this;
+
+        while (!history.AllZero)
+        {
+            children.Push(history);
+            history = new History(history);
+        }
+
+        var sum = 0L;
+        var lastValue = history.LatestValue;
+        while (children.TryPop(out var child))
+        {
+            lastValue = child.LatestValue + lastValue;
+            sum += lastValue;
+        }
+
+        return lastValue;
+    }
+
+    public bool AllZero => _values.All(v => v == 0L);
+
+    public long LatestValue => _values[^1];
+}
---
+++ Day9/Program.cs
@@ -0,0 +1,11 @@
+﻿using Day9;
+
+void PartOne()
+{
+    var histories = File.ReadAllLines("input.txt").Select(line => new History(line)).ToArray();
+    var sum = histories.Sum(h => h.ExtrapolateNextValue());
+
+    Console.WriteLine($"Sum of extrapolated values: {sum}");
+}
+
+PartOne();
