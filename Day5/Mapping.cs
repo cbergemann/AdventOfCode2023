6123f1958071e43949a75dbbe65d230923277826
cbergemann <19253536+cbergemann@users.noreply.github.com>
2023-12-09 10:00:52 +0100

    add Day5 part 1 solution

---
+++ Day5/Mapping.cs
@@ -0,0 +1,46 @@
+﻿namespace Day5;
+
+public class Mapping
+{
+    private List<(long, long, long)> _entries;
+    
+    public Mapping(string description, Queue<string> lines)
+    {
+        var mappingName = description.Substring(0, description.Length - 5).Split("-to-");
+        SourceName = mappingName[0];
+        DestinationName = mappingName[1];
+
+        _entries = new List<(long, long, long)>();
+        var mappingEntry = lines.Dequeue();
+        while (mappingEntry.Length > 0)
+        {
+            var mapNumbers = mappingEntry.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+            _entries.Add((mapNumbers[0], mapNumbers[1], mapNumbers[2]));
+
+            if (!lines.TryDequeue(out mappingEntry))
+            {
+                break;
+            }
+        }
+    }
+    
+    public string SourceName { get; }
+    
+    public string DestinationName { get; }
+
+    public long this[long value]
+    {
+        get
+        {
+            foreach (var (destinationRageStart, sourceRangeStart, rangeSize) in _entries)
+            {
+                if (value >= sourceRangeStart && value < sourceRangeStart + rangeSize)
+                {
+                    return destinationRageStart + value - sourceRangeStart;
+                }
+            }
+
+            return value;
+        }
+    }
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day5;
+
+void PartOne()
+{
+    var initalSeeds = new List<long>();
+    var mappings = new Dictionary<string, Mapping>();
+    
+    var lines = new Queue<string>(File.ReadAllLines("input.txt"));
+    while (lines.TryDequeue(out var line))
+    {
+        if (line.StartsWith("seeds: "))
+        {
+            initalSeeds = line.Substring(7).Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse)
+                .ToList();
+            continue;
+        }
+
+        if (line.EndsWith(" map:"))
+        {
+            var mapping = new Mapping(line, lines);
+            mappings.Add(mapping.SourceName, mapping);
+        }
+    }
+
+    long GetLocation(long seed)
+    {
+        var currentType = "seed";
+        var value = seed;
+        while (currentType != "location")
+        {
+            var map = mappings[currentType];
+            currentType = map.DestinationName;
+            value = map[value];
+        }
+
+        return value;
+    }
+
+    var closestSeedLocation = initalSeeds.Min(GetLocation);
+    
+    Console.WriteLine($"the closest seed location is: {closestSeedLocation}");
+}
+
+PartOne();
