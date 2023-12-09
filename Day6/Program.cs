fdd1ef552bec8fd7075450db27d60f1242a45469
cbergemann <19253536+cbergemann@users.noreply.github.com>
2023-12-09 11:33:31 +0100

    add Day 6 solution

---
+++ Day6/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using Day6;
+
+void PartOne()
+{
+    // var races = new Race[]
+    // {
+    //     new(7, 9),
+    //     new(15, 40),
+    //     new(30, 200),
+    // };
+    
+    var races = new Race[]
+    {
+        new(44, 283),
+        new(70, 1134),
+        new(70, 1134),
+        new(80, 1491),
+    };
+
+    var prod = races.Aggregate(1, (prod, race) => prod * race.NumberSolutions());
+
+    Console.WriteLine($"number of ways to beat the race aggregated: {prod}");
+}
+
+void PartTwo()
+{
+    // var race = new Race(71530, 940200);
+    var race = new Race(44707080, 283113411341491);
+    
+    Console.WriteLine($"number of ways to beat the race: {race.NumberSolutions()}");
+}
+
+PartOne();
+PartTwo();
---
+++ Day6/Race.cs
@@ -0,0 +1,34 @@
+﻿namespace Day6;
+
+public class Race
+{
+    private readonly int _duration;
+    private readonly long _record;
+
+    public Race(int duration, long record)
+    {
+        _duration = duration;
+        _record = record;
+    }
+
+    public int NumberSolutions()
+    {
+        var solutions = 0;
+        for (var it = 0; it < _duration; it++)
+        {
+            var timeLeft = (long)(_duration - it);
+            var distance = timeLeft * it;
+
+            if (distance > _record)
+            {
+                solutions++;
+            }
+            else if (solutions > 0)
+            {
+                break;
+            }
+        }
+
+        return solutions;
+    }
+}
